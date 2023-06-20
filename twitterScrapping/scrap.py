04b6932cdae270d76e62bd71a0dbbc96ef4b22a0
Guilherme Bernardo <bguilherme51@gmail.com>
2023-06-19 21:18:09 -0400

    benchmarks

---
+++ twitterScrapping/scrap.py
@@ -0,0 +1,17 @@
+import snscrape.modules.twitter as sntwitter
+import pandas as pd
+
+hashtag = "#roraima"
+min_likes = 50
+
+tweets = []
+for i, tweet in enumerate(sntwitter.TwitterSearchScraper(f'{hashtag}').get_items()):
+    if i >= 1000:  # Scrape 1000 tweets
+        break
+    if tweet.likeCount >= min_likes:
+        tweets.append([tweet.date, tweet.user.username, tweet.likeCount, tweet.sourceLabel, tweet.content])
+
+# Convert the list of tweets to a DataFrame
+tweets_df = pd.DataFrame(tweets, columns=["Date Created", "User", "Number of Likes", "Source of Tweet", "Tweet"])
+
+print(tweets_df.head())
